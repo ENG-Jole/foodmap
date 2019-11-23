d3e02d6c8d7153d46a9ec8b117d04fba52689066
Enny Jole <engjole@gmail.com>
2019-11-23 02:03:36 -0800

    Using router instead of get

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-app.get(‘/’, function(req, res) {
-  res.send(‘Hello World’)
-}
+const path = __dirname + '/content/';
+const port = 8080;
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
