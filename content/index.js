38d0fd6f8816ed8cee3ba7cc1404fa610e49330d
Enny Jole <engjole@gmail.com>
2019-12-17 22:15:07 -0800

    Attempt at API + dynamic table

---
+++ content/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const apiUrl = "https://food.engjole.net";
+
+function App() {
+  const [items, setItems] = React.useState([]);
+  const columns = [{
+    dataField: 'id',
+    text: 'Restaurant ID'
+    }, {
+    dataField: 'name',
+    text: 'Restaurant Name'
+    }, {
+    dataField: 'pricerange',
+    text: 'Price'
+    }];
+  React.useEffect(() => {
+    async function fetchData() {
+      var data = await fetch(apiUrl).then(res => {
+        return res.json();
+      });
+      setItems(data);
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div class="container">
+      <BootstrapTable keyField='id' data={ items } columns={ columns } />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
