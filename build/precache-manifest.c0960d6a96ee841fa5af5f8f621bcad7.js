1df0900e6bffd8acaf08f9b4edb23488c98b0396
Enny Jole <engjole@gmail.com>
2019-12-18 23:46:31 -0800

    new build location

---
+++ build/precache-manifest.c0960d6a96ee841fa5af5f8f621bcad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9025eb23876d8ce55c7411a5ec585023",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5eefac0bdbbf8f3626c9",
+    "url": "./static/css/2.14df46f2.chunk.css"
+  },
+  {
+    "revision": "36731690ba7d9e3e2bfa",
+    "url": "./static/css/main.7f02dc24.chunk.css"
+  },
+  {
+    "revision": "5eefac0bdbbf8f3626c9",
+    "url": "./static/js/2.fe1bc168.chunk.js"
+  },
+  {
+    "revision": "e76f6fc05df4c6012490e86628397d0a",
+    "url": "./static/js/2.fe1bc168.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36731690ba7d9e3e2bfa",
+    "url": "./static/js/main.e1069bee.chunk.js"
+  },
+  {
+    "revision": "5e16ef3a29422b2ca18e",
+    "url": "./static/js/runtime-main.bbec7e85.js"
+  }
+]);
