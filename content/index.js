d955896b13ea8be08af64fe72066631157dea4fb
Enny Jole <engjole@gmail.com>
2019-12-18 21:03:33 -0800

    Adding working react table

--- content/index.js
+++ content/index.js
@@ -1,41 +1,8 @@
-const React = require('React');
-const Component = React.Component();
-const Reactdom = require('react-dom');
-const render = Reactdom.render();
-const reactbootstraptable = require('react-bootstrap-table-next');
-const BootstrapTable = reactbootstraptable.BootstrapTable();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const apiUrl = "https://food.engjole.net/labreak";
-
-function App() {
-  const [items, setItems] = React.useState([]);
-  const columns = [{
-    dataField: 'id',
-    text: 'Restaurant ID'
-    }, {
-    dataField: 'name',
-    text: 'Restaurant Name'
-    }, {
-    dataField: 'pricerange',
-    text: 'Price'
-    }];
-  React.useEffect(() => {
-    async function fetchData() {
-      var data = await fetch(apiUrl).then(res => {
-        return res.json();
-        if (error) {
-          throw error
-        }
-      });
-      setItems(data);
-      console.log(data);
-    }
-    fetchData();
-  }, []);
-return(
-    <div class="container">
-      <BootstrapTable keyField='id' data={ items } columns={ columns }/>
-    </div>
-  );
-}
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
